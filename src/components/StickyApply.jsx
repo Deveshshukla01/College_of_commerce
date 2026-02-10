c3aeac94f0fe054821349609a0f288cd57e10c5a
Deveshshukla01 <deveshshukla9915@gmail.com>
2026-02-10 19:11:03 +0530

    stikcy footer

--- src/components/StickyApply.jsx
+++ src/components/StickyApply.jsx
@@ -17,7 +17,7 @@ export default function StickyApply() {
   return (
     <Link
       to="/apply"
-      className="fixed bottom-0 w-full bg-[#edc98f] text-black py-2 overflow-hidden rounded-t-2xl shadow-lg"
+      className="fixed bottom-0 w-full bg-red-600 text-white py-2 overflow-hidden rounded-t-2xl shadow-lg"
     >
       <div className="animate-marquee whitespace-nowrap text-center font-bold text-lg">
         Apply Now – Admission Form Open
